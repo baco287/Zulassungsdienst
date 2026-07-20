abb045498ecf12d389fc0e34e75b5bb7da22de9d
baco287 <baco287@users.noreply.github.com>
2026-07-20 10:32:35 +0000

    deploy: 892302d07dfd3800b8fdf6a776ad96158858f137

---
+++ _next/static/chunks/app/auftrag/page-680c565ff81181fe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[827],{1883:(e,s,_)=>{Promise.resolve().then(_.bind(_,4582))}},e=>{e.O(0,[298,582,441,255,358],()=>e(e.s=1883)),_N_E=e.O()}]);
--- _next/static/chunks/app/auftrag/page-a47da1512d7a5e46.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[827],{1883:(e,s,_)=>{Promise.resolve().then(_.bind(_,2770))}},e=>{e.O(0,[261,770,441,255,358],()=>e(e.s=1883)),_N_E=e.O()}]);
