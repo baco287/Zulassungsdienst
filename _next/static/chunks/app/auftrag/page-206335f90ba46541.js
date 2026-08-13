528d9fdb622a4fb2d7dd1959d254f91b572c79d4
mastermsc-h <mastermsc-h@users.noreply.github.com>
2026-08-13 18:59:16 +0000

    deploy: 38d0898ae14dcb10b36a70cacfbe49ec3bd61013

--- _next/static/chunks/app/auftrag/page-206335f90ba46541.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[827],{1883:(e,s,_)=>{Promise.resolve().then(_.bind(_,7813))}},e=>{e.O(0,[298,813,441,255,358],()=>e(e.s=1883)),_N_E=e.O()}]);
